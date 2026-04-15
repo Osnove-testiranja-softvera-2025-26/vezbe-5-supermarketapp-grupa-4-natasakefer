efd4b8bf13ad0fb9f45425862014db60e55dab58
Natasa <kefernatasa909@gmail.com>
2026-04-15 12:21:19 +0200

    Vezbe 5 - Prvi test

--- OTS2019_Supermarket.Test/CartTest.cs
+++ OTS2019_Supermarket.Test/CartTest.cs
@@ -8,8 +8,19 @@ using System.Threading.Tasks;
 
 namespace OTS_Supermarket.Test
 {
+    [TestFixture]
     public class CartTest
     {
-
+        [Test]
+        public void AddOneToCart_ShouldAddItemToCart_Success()
+        {
+            //ARRANGE
+            Cart cart = new Cart();
+            Monitor monitor = new Monitor("Dell", 200);
+            //ACT
+            cart.AddOneToCart(monitor);
+            //ASSERT
+            Assert.That(cart.Size, Is.EqualTo(1));
+        }
     }
 }
